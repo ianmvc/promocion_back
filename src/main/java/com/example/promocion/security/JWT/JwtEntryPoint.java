7da464c6ce613047a66d637055cca2a763a613c1
ianmvc <ianmvc4@gmail.com>
2021-03-10 17:32:41 -0600

    MensajeLogin

--- src/main/java/com/example/promocion/security/JWT/JwtEntryPoint.java
+++ src/main/java/com/example/promocion/security/JWT/JwtEntryPoint.java
@@ -17,7 +17,7 @@ public class JwtEntryPoint implements AuthenticationEntryPoint {
     private static  final Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
     @Override
     public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException e) throws IOException, ServletException {
-        logger.error("fail en el método commence");
+        logger.error("Error");
         res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "credenciales erróneas");
     }
 }
